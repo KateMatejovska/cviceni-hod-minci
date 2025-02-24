96e6ecf641e90b8f64f881540c37174f92bdf103
Katerina Matejovska <katerinamatejovska@email.cz>
2025-02-24 20:24:11 +0100

    oprava1

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const padlOrel = Math.random() < 0.5;
 
+const minceEl = document.querySelector('.mince');
 const vysledekEl = document.querySelector('.vysledek');
 
 if (padlOrel) {
     vysledekEl.textContent = 'Padl orel';
-} else {
-    vysledekEl.textContent = 'Padla panna'
-}  
-
-const minceEl = document.querySelector('.mince');
-if (padlOrel) {
     minceEl.classList.add('mince--orel');
 } else {
+    vysledekEl.textContent = 'Padla panna'
     minceEl.classList.add('mince--panna');
-}
+}  
+
